5afd1741437831e52b25d963dfeea1a83fe8fb26
EC2 Default User <ec2-user@ip-172-31-94-199.ec2.internal>
2022-09-05 16:41:01 +0000

    circleci badge

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<include a CircleCI status badge, here>
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/jude-don/DevOps_Operationalizing-ML/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/jude-don/DevOps_Operationalizing-ML/tree/main)
 
 ## Project Overview
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=jude-don/devops_operationalizing
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker --tag devops_operationalizing $dockerpath:latest
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
